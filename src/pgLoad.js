c1ca9428fb3fc3fb42ac2644f16556356f309a93
sabrinaMac <sabrina.scherma@gmail.com>
2022-10-10 15:05:07 -0600

    built DOM elements

--- src/pgLoad.js
+++ src/pgLoad.js
@@ -3,7 +3,6 @@ import frontImg from './chefImg.jpg';
 
 const content = document.getElementById('content');
 
-
 export default function buildPage() {
     const bgImg = new Image();
     const chefImg = new Image();
@@ -11,45 +10,98 @@ export default function buildPage() {
     chefImg.src = frontImg;
 
     // Div creation for header
+    const content = document.createElement('div');
     const imgDiv = document.createElement('div');
-    const menuDiv = document.createElement('div');
+    const headerDiv = document.createElement('div');
+    const headerTop = document.createElement('div');
+    const headerBot = document.createElement('div');
     const headline = document.createElement('div');
     const home = document.createElement('button');
     const menu = document.createElement('button');
     const contact = document.createElement('button');
 
-    //Div creation for main 
-    const main = document.createElement('div');
-    const mainP = document.createElement('p');
-    const mainImg = document.createElement('div');
+    //Div creation for home 
+    const homeDiv = document.createElement('div');
+    const homePDiv = document.createElement('div');
+    const homeMainP1 = document.createElement('p');
+    const homeMainP2 = document.createElement('p');
+    const homeMainImg = document.createElement('div');
+
+    //Div creation for menu
+    const menuDiv = document.createElement('div');
+    const menuItemsDiv = document.createElement('div');
+    const menuItemsP = document.createElement('p');
+
+    //Div creation for contact
+    const contactDiv = document.createElement('div');
+    const contactItemsDiv = document.createElement('div');
+    const git = document.createElement('a');
+    const linked = document.createElement('a');
+    const email = document.createElement('a');
+
 
     //Appending divs to content div
+    document.body.appendChild(content);
     content.appendChild(imgDiv);
+    content.appendChild(headerDiv);
+    content.appendChild(homeDiv);
     content.appendChild(menuDiv);
-    content.appendChild(main);
+    content.appendChild(contactDiv);
 
-    menuDiv.appendChild(headline);
-    menuDiv.appendChild(home);
-    menuDiv.appendChild(menu);
-    menuDiv.appendChild(contact);
+    headerDiv.appendChild(headerTop);
+    headerDiv.appendChild(headerBot);
+    headerTop.appendChild(headline);
+    headerBot.appendChild(home);
+    headerBot.appendChild(menu);
+    headerBot.appendChild(contact);
 
-    main.appendChild(mainP);
-    main.appendChild(mainImg);
+    homeDiv.appendChild(homePDiv);
+    homePDiv.appendChild(homeMainP1);
+    homePDiv.appendChild(homeMainP2);
+    homeDiv.appendChild(homeMainImg);
+
+    menuDiv.appendChild(menuItemsDiv);
+    menuItemsDiv.appendChild(menuItemsP);
+
+    contactDiv.appendChild(contactItemsDiv);
+    contactItemsDiv.appendChild(git);
+    contactItemsDiv.appendChild(linked);
+    contactItemsDiv.appendChild(email);
 
     //Adding classlists 
     home.classList.add('button');
     menu.classList.add('button');
     contact.classList.add('button');
+    headerDiv.classList.add('bg');
+    headerDiv.classList.add('headerDiv');
+    headline.classList.add('headline');
+    headerTop.classList.add('headerTop');
+    headerBot.classList.add('headerBot');
+    homeDiv.classList.add('bg');
+    homeDiv.classList.add('main');
+    homePDiv.classList.add('homePDiv');
+    homeMainImg.classList.add('mainImg');
+    content.classList.add('contentDiv');
     menuDiv.classList.add('bg');
-    menuDiv.classList.add('menuDiv');
-    main.classList.add('bg');
-    main.classList.add('main');
+    menuDiv.classList.add('main');
+    menuDiv.classList.add('hidden');
+    contactDiv.classList.add('bg');
+    contactDiv.classList.add('main');
+    contactDiv.classList.add('hidden');
 
     //Adding IDs
+    home.setAttribute('id', 'homeBtn');
+    menu.setAttribute('id', 'menuBtn');
+    contact.setAttribute('id', 'contactBtn')
 
     //Appending text
     home.innerHTML = 'Home';
     menu.innerHTML = 'Menu';
     contact.innerHTML = 'Contact';
+    headline.innerHTML = 'Restaurant';
+    homeMainP1.innerHTML = 'Welcome to Restaurant! We offer indoor and outdoor dining, as well as takeout and deliver options!';
+    homeMainP2.innerHTML = 'To place a takeout or deliver order, call 123-456-7890';
 
+    //Appending images
+    homeMainImg.appendChild(chefImg)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
 module.exports = {
   // remove this eventually
   mode: 'development',
-  
   entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Restaurant',
     }),
   ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
